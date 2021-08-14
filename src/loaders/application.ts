2fbb7c0e207c0f50f6d6c5f0619c671c7b3427d0
Ofir Shavit <ofirshavit100@gmail.com>
2021-08-14 21:15:19 +0300

    routing structre

--- src/loaders/application.ts
+++ src/loaders/application.ts
@@ -1,11 +1,15 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import logger from 'morgan';
+import router from '../routes';
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(logger('dev'));
+
+// Routing endpoints
+app.use(router);
 
 export default app;
